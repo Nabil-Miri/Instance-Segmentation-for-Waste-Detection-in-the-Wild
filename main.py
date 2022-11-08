929f4ea3c22bafb17038ef7a0061748639f99a3e
Nabil-Miri <75530842+Nabil-Miri@users.noreply.github.com>
2022-11-08 16:50:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,61 +10,50 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer, ColorMode
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.data.catalog import Metadata
+from PIL import Image, ImageOps
 ''''''
-_dataset_metadata_train = MetadataCatalog.get('train')
+# Detectron2 Setup Logger
+setup_logger()
+
+# Detectron2 Metadata
 my_metadata = Metadata()
 my_metadata.set(thing_classes=['metals_and_plastics', 'other', 'non-recyclable', 'glass', 'paper', 'bio', 'unknown'])
 cats = ['metals_and_plastics', 'other', 'non-recyclable', 'glass', 'paper', 'bio', 'unknown']
-# print(torch.__version__, torch.cuda.is_available())
-# assert torch.__version__.startswith("1.8")
 
-setup_logger()
-
-st.title('Deploying a Pytorch model on Streamlit | Detectron2')
-st.write('What is Detectron2?')
-st.write('Detectron2 is Facebook AI Researchs next generation software system that implements state-of-the-art object detection algorithms. It is a ground-up rewrite of the previous version, Detectron, and it originates from maskrcnn-benchmark.')
-st.image('assets/img.png')
-
-from PIL import Image, ImageOps
-
-# import some common detectron2 utilities
+# Streamlit Title
+st.title('Waste Detector:')
+st.caption('Using Detectron2 (BBox+Segmentation)')
 
 st.write('\n')
 
-st.title('Testing the Zoo Model')
-st.write('Test image')
-im = cv2.imread("assets/test.JPG")
+# st.write('Test image')
+# im = cv2.imread("assets/test.JPG")
+# # showing image
+# st.image('assets/test.JPG')
 
-# showing image
-st.image('assets/test.JPG')
+# Import some common detectron2 utilities
 NUM_CLASSES = 7
 #Then, we create a detectron2 config and a detectron2 `DefaultPredictor` to run inference on this image.
 cfg = get_cfg()
 cfg.MODEL.DEVICE = "cpu"
-# print(cfg)
-# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.3  # set threshold for this model
-# #Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-##cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = NUM_CLASSES
+# Custom Trained Model
 cfg.MODEL.WEIGHTS = os.path.join("model_final.pth")
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512 #128
 
+# minimum image size for the train set
+cfg.INPUT.MIN_SIZE_TRAIN = (800,)
+# maximum image size for the train set
+cfg.INPUT.MAX_SIZE_TRAIN = 1333
+# minimum image size for the test set
+cfg.INPUT.MIN_SIZE_TEST = 800
+# maximum image size for the test set
+cfg.INPUT.MAX_SIZE_TEST = 1333
+
+# Predictor
 predictor = DefaultPredictor(cfg)
-outputs = predictor(im)
-
-st.write('Writing pred_classes/pred_boxes output ===============')
-st.write(outputs["instances"].pred_classes)
-st.write(outputs["instances"].pred_boxes)
-
-st.write('Using Vizualizer to draw the predictions on Image')
-
-v = Visualizer(im[:, :, ::-1], metadata=my_metadata, instance_mode=ColorMode.IMAGE_BW, # removes the colors of unsegmented pixels
-                scale=1.2)
-out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-st.image(out.get_image()[:, :, ::-1])
-
 
 ###
 # Upload images
@@ -73,18 +62,17 @@ upload = st.file_uploader("Please upload an image", type=["jpg","png", "jpeg", "
 if upload is None:
     st.text("Please upload an image")
 else:
+    st.write('Image Uploaded:')
     image = Image.open(upload)
     st.image(image, use_column_width=True)
     img_array = np.array(image)
     outputs = predictor(img_array)
 
-    st.write('Writing pred_classes/pred_boxes output ===============')
-    st.write(outputs["instances"].pred_classes)
-    st.write(cats)
-    st.write(outputs["instances"].pred_boxes)
-    st.write(outputs["instances"].scores)
-    st.write(outputs)
-    st.write('Width:', outputs["instances"].image_width)
+    st.title('Prediction Outputs:')
+    st.write('Pred Classes: ',outputs["instances"].pred_classes)
+    st.write('Categories: ',cats)
+    # st.write('Pred Boxes: ',outputs["instances"].pred_boxes)
+    st.write('Scores: ', outputs["instances"].scores)
 
     st.write('Using Vizualizer to draw the predictions on Image')
 
