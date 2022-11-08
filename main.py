28fa255069b74765f98a4b3e64a058c6d539be8e
Nabil-Miri <75530842+Nabil-Miri@users.noreply.github.com>
2022-11-08 15:32:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26,14 +26,6 @@ st.write('Detectron2 is Facebook AI Researchs next generation software system th
 st.image('assets/img.png')
 
 from PIL import Image, ImageOps
-# Upload images
-st.write('Upload image')
-upload = st.file_uploader("Please upload an image", type=["jpg","png", "jpeg", "heif"])
-if upload is None:
-    st.text("Please upload an image")
-else:
-    image = Image.open(upload)
-    st.image(image, use_column_width=True)
 
 # import some common detectron2 utilities
 
@@ -75,17 +67,25 @@ st.image(out.get_image()[:, :, ::-1])
 
 
 ###
-img_array = np.array(image)
-outputs = predictor(img_array)
+# Upload images
+st.write('Upload image')
+upload = st.file_uploader("Please upload an image", type=["jpg","png", "jpeg", "heif"])
+if upload is None:
+    st.text("Please upload an image")
+else:
+    image = Image.open(upload)
+    st.image(image, use_column_width=True)
+    img_array = np.array(image)
+    outputs = predictor(img_array)
 
-st.write('Writing pred_classes/pred_boxes output ===============')
-st.write(outputs["instances"].pred_classes)
-st.write(outputs["instances"].pred_boxes)
+    st.write('Writing pred_classes/pred_boxes output ===============')
+    st.write(outputs["instances"].pred_classes)
+    st.write(outputs["instances"].pred_boxes)
 
-st.write('Using Vizualizer to draw the predictions on Image')
+    st.write('Using Vizualizer to draw the predictions on Image')
 
-v = Visualizer(img_array[:, :, ::-1], metadata=my_metadata, instance_mode=ColorMode.IMAGE_BW, # removes the colors of unsegmented pixels
-                scale=1.2)
-out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-st.image(out.get_image()[:, :, ::-1])
+    v = Visualizer(img_array[:, :, ::-1], metadata=my_metadata, instance_mode=ColorMode.IMAGE_BW, # removes the colors of unsegmented pixels
+                    scale=1.2)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    st.image(out.get_image()[:, :, ::-1])
 ''''''
